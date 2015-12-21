f680aeec3b1e8ed80c603b0eb49a485af3ef0511
biancarosa <me@biancarosa.com.br>
2015-12-21 15:19:51 -0200

    Turning graph into a list

--- PathChallenge.java
+++ PathChallenge.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Scanner;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -17,7 +20,8 @@ public class PathChallenge {
         if (w == goal) {
             return path;
         }
-        for (int i = 1; i <= graph.getVertices(); i++) {
+
+        for (Integer i : graph.getVertices()) {
             if (graph.hasPathBetween(w,i)) {
                 if (!visited[i]) {
                     List returnPath = findPath(i, goal, path);
@@ -31,20 +35,20 @@ public class PathChallenge {
     }
 
     public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
         GraphInputHandler inputHandler = new GraphInputHandler();
 
         try {
             System.out.print("Start node :: ");
-            Integer start = Integer.parseInt(reader.readLine());
+            Integer start = scanner.nextInt();
 
             System.out.print("End node :: ");
-            Integer end = Integer.parseInt(reader.readLine());
+            Integer end =  scanner.nextInt();
 
-            graph = inputHandler.readInput(reader);
+            graph = inputHandler.readInput(scanner);
 
-            visited = new boolean[graph.getVertices()+1];
-            for (int i = 0; i < graph.getVertices(); ++i) {
+            visited = new boolean[graph.getNumberOfVertices()+1];
+            for (int i = 0; i < graph.getNumberOfVertices(); ++i) {
                 visited[i] = false;
             }
             List path = findPath(start, end, new LinkedList());
@@ -63,60 +67,66 @@ public class PathChallenge {
 }
 
 class Graph {
-    private boolean graph[][];
-    private int vertices;
+    private Map<Integer, LinkedList<Integer>> graph;
+    private List<Integer> vertices;
     private int edges;
 
-    Graph(int vertices) {
-        this.vertices = vertices;
-        graph = new boolean[vertices+1][vertices+1];
+    Graph() {
+        graph = new HashMap<Integer, LinkedList<Integer>>();
+        vertices = new LinkedList<Integer>();
+        edges = 0;
     }
 
     public boolean hasPathBetween(int u, int v) {
-        return this.graph[u][v];
+        LinkedList<Integer> list = graph.get(u);
+        if (list != null) {
+            return list.contains(v);
+        }
+        return false;
     }
 
     public void addEdge(int u, int v) {
-        this.graph[u][v] = true;
+        LinkedList<Integer> list;
+        if (graph.get(u) == null) {
+            list = new LinkedList<Integer>();
+            graph.put((Integer) u, list);
+            vertices.add(u);
+        } else {
+            list = graph.get(u);
+        }
+        list.add(v);
+        vertices.add(v);
+        edges++;
     }
 
-    public int getVertices() {
+    public List<Integer> getVertices() {
         return this.vertices;
     }
 
-    public int getEdges() {
-        return this.edges;
+    public int getNumberOfVertices() {
+        return this.vertices.size();
     }
 
-    void print() {
-        for (int i = 1; i < vertices; ++i) {
-            for (int j = 1; j < vertices; ++j) {
-                System.out.print(graph[i][j]+"  ");
-            }
-            System.out.print("\n");
-        }
+    public int getNumberOfEdges() {
+        return this.edges;
     }
 
 }
 
 
 class GraphInputHandler {
-    public Graph readInput(BufferedReader reader) throws IOException {
-        System.out.print("Number of vertices: ");
-        Integer vertices = Integer.parseInt(reader.readLine());
+    public Graph readInput(Scanner scanner) throws IOException {
 
-        Graph graph = new Graph(vertices);
+        Graph graph = new Graph();
 
-        System.out.print("Number of edges: ");
-        Integer edges = Integer.parseInt(reader.readLine());
+        System.out.print("Write number of edges :: ");
+        Integer edges = scanner.nextInt();
 
-        System.out.println("Write edges separated by a comma, one per line.");
+        System.out.println("Write vertices for each edge (1 1) ");
         for (int i = 1; i <= edges; ++i) {
             System.out.print("Edge #" + i + " ");
-            String line = reader.readLine();
-            String[] splitted = line.split(",");
-            graph.addEdge(Integer.parseInt(splitted[0]), Integer.parseInt(splitted[1]));
-        }
+            graph.addEdge(scanner.nextInt(), scanner.nextInt());
+        };
 
         return graph;
     }
